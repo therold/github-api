e23fc7e2b3e48030f4d19af3d2fa4342e2cd967a
Timothy Herold <heroldt@gmail.com>
2016-10-13 20:13:35 -0700

    Add support for parsing form input to github-interface.js

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,6 +1,11 @@
-var Github = require('./../js/github.js').githubModule;
+const Github = require('./../js/github.js').githubModule;
+const queryString = require('query-string');
 var github = new Github();
 
 $(document).ready(function() {
-  
+  var search = queryString.parse(location.search).name;
+  if(search) {
+    $('#name').val(search);
+    github.search(search);
+  }
 });
