c2b8dbfcc5234006d3556f91389fff89470a2a43
Timothy Herold <heroldt@gmail.com>
2016-10-13 19:50:47 -0700

    First pass at basic user search function.

--- js/github.js
+++ js/github.js
@@ -5,4 +5,13 @@ function Github() {
 
 }
 
+Github.prototype.search = function(search) {
+  $.get(`${baseURL}${search}?access_token=${apiKey}`)
+    .then(function(response) {
+      console.log(response);
+    }).fail(function(error) {
+      console.log(error.responseJSON.message);
+    });
+};
+
 exports.githubModule = Github;
