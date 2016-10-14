4d8287bd5eb8ae2cdd3e6b00f78273496627f8e1
Timothy Herold <heroldt@gmail.com>
2016-10-14 08:07:43 -0700

    Simplify search functions a bit.

--- js/search-interface.js
+++ js/search-interface.js
@@ -6,23 +6,32 @@ displaySearchResults = function(result) {
     $('.results').append(resultInfo);
   } else {
     result.items.forEach(function(item, i) {
-      var userInfo = `
-        <div class="userInfo">
-          <img src=${item.avatar_url} class="pull-right avatarImg"></img>
-          <p>${item.login}</p>
-          <div class="userDetails user${i}">
-            <h3 class='loadingUserDetails text-center'>Loading...</h3>
-          </div>
-          <hr>
-        </div>`;
+      var userInfo = parseUser(item, i);
       $('.results').append(userInfo);
       $('.userInfo').last().click(function() {
-        $('.userDetailsData').remove();
-        $(this).find('.loadingUserDetails').show();
-        github.user(item.login, i, displaySearchDetails, displaySearchDetailsError);
+        loadSearchDetails(item.login, i);
       });
     });
   }
+};
+
+parseUser = function(user, n) {
+  var userInfo = `
+    <div class="userInfo">
+      <img src=${user.avatar_url} class="pull-right avatarImg"></img>
+      <p>${user.login}</p>
+      <div class="userDetails user${n}">
+        <h3 class='loadingUserDetails text-center'>Loading...</h3>
+      </div>
+      <hr>
+    </div>`;
+  return userInfo;
+};
+
+loadSearchDetails = function(user, n) {
+  $('.userDetailsData').remove();
+  $(`user${n}`).find('.loadingUserDetails').show();
+  github.user(user, n, displaySearchDetails, displaySearchDetailsError);
 };
 
 displaySearchError = function(error) {
