5ea78327e91828405d1ae3f4471d288a2efbf1dc
Timothy Herold <heroldt@gmail.com>
2016-10-13 20:22:56 -0700

    Adjust search function to actually search instead of just seeking out an exact match.

--- js/github.js
+++ js/github.js
@@ -1,12 +1,12 @@
 const apiKey = require('./../.env').apiKey;
-const baseURL = "https://api.github.com/users/";
+const baseURL = "https://api.github.com";
 
 function Github() {
 
 }
 
 Github.prototype.search = function(search) {
-  $.get(`${baseURL}${search}?access_token=${apiKey}`)
+  $.get(`${baseURL}/search/users?q=${search}&access_token=${apiKey}`)
     .then(function(response) {
       console.log(response);
     }).fail(function(error) {
